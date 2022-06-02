b204f3cd8fd7a06231c7f1d184c0fa35141f17a9
crisfordy2 <gomez18cristian@gmail.com>
2022-06-01 20:23:59 -0500

    add 11 to 16

--- clase_pandas.py
+++ clase_pandas.py
@@ -16,7 +16,6 @@ data.loc[data['Estado'] == 'LEVE'] = 'Leve'
 cantidad_casos = data.shape[0]
 print("1.Cantidad de contagios: ", cantidad_casos)
 
-#Nombre municipio
 numero_municipios = data['Nombre municipio'].value_counts().count()
 print("2.Cantidad de municipios afectados: ", cantidad_casos)
 
@@ -44,8 +43,26 @@ print("9. lista departamentos afectados: ", lista_departamento)
 tipo_atencion_mayor_menor = data['Ubicación del caso'].sort_values(ascending=False)
 print("10. Ordene de mayor a menor por tipo de atención: ", tipo_atencion_mayor_menor)
 
-departamentos_mayores_casos = data['Nombre municipio' ].value_counts().head(10)
-print("11. 10departamentos con mas casos de contagiados: ", departamentos_mayores_casos)
+departamentos_mayores_casos = data['Nombre departamento' ].value_counts().head(10)
+print("11. 10 departamentos con mas casos de contagiados: ", departamentos_mayores_casos)
+
+departamentos_mayores_fallesidos = data[data['Estado'] == 'Fallecido'].groupby('Nombre departamento').size().sort_values(ascending=False).head(10)
+print("12. 10 departamentos con mas casos de fallesidos: ", departamentos_mayores_casos)
+
+departamentos_mayores_recuperado = data[data['Recuperado'] == 'Recuperado'].groupby('Nombre departamento').size().sort_values(ascending=False).head(10)
+print("13. 10 departamentos con mas casos de recuperados: ", departamentos_mayores_recuperado)
+
+departamentos_mayores_contagios = data['Nombre municipio'].value_counts().head(10)
+print("14. 10 departamentos con mas casos de constagios: ", departamentos_mayores_contagios)
+
+municipio_mayores_fallecidos = data[data['Estado'] == 'Fallecido'].groupby('Nombre municipio').size().sort_values(ascending=False).head(10)
+print("15. 10 municipios con mas casos de fallecidos: ", municipio_mayores_fallecidos)
+
+municipio_mayores_recuperado = data[data['Recuperado'] == 'Recuperado'].groupby('Nombre municipio').size().sort_values(ascending=False).head(10)
+print("16.  10 municipios con mas casos de recuperados: ", municipio_mayores_recuperado)
+
+
+
 
 
 
@@ -55,6 +72,7 @@ data.loc[data['Sexo'] == 'm'] = 'M'
 data.loc[data['Sexo'] == 'f'] = 'F'
 
 # Cuantas mujeres fallecieron en Colombia
+aux = data.loc[(data['Estado'] == 'Fallecido') & (data['Sexo'] == 'F') ]
 aux = data.loc[(data['Estado'] == 'Fallecido') & (data['Sexo'] == 'F') ]
 cantidad_muertes_mujeres = aux.shape[0]
 
